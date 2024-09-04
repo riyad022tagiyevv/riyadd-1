1e27de5feb1af65c6bb5c80a4ee38a0fa4e25006
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2024-09-04 10:09:17 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_OWNER = os.environ.get("BOT_OWNER")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME")
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID"))
+    BOT_TOKEN = os.environ.get("7351555728:AAGCPqgaw7V26cSUER9qQyt_Q7gBDhhhy2Y")
+    API_ID = int(os.environ.get("21739502"))
+    API_HASH = os.environ.get("810e4e27a80123e48c122e95b81392eb")
+    BOT_OWNER = os.environ.get("thagiyev")
+    BOT_USERNAME = os.environ.get("nezrinsongbot")
+    PLAYLIST_NAME = os.environ.get("nezrinsupp")
+    PLAYLIST_ID = int(os.environ.get("-1002168356385"))
